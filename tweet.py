6e321f70afd360af1ac8c516847650bca0bba997
linnie <omgjc71@gmail.com>
2021-03-20 14:04:56 -0400

    initial

--- tweet.py
+++ tweet.py
@@ -8,10 +8,10 @@ def get_api(cfg):
 
 def main(compose):
     cfg = {
-        "consumer_key"          : "X7qJ71E3WjBZypUSOsqs2ITTf",
-        "consumer_secret"       : "PovSLMkdpvUUHdTuwNvPhoLWlvXME8NIzkESBeuNoTl64BVkjU",
-        "access_token"          : "771072247385710592-2gdWUsJ5BT6V9gfDOPz4AmPEENpGWvu",
-        "access_token_secret"   : "zGgLKKPvzBxfdeTMcIAj46g2132pXZWiygUhhlmZuJ0uP"
+        "consumer_key"          : "",
+        "consumer_secret"       : "",
+        "access_token"          : "",
+        "access_token_secret"   : ""
     }
 
     api = get_api(cfg)
